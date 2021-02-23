a35758cbdd28a3d5b536924559152444a81375f5
drpiotr <79456861+drpiotr@users.noreply.github.com>
2021-02-23 15:24:45 +0100

    dzein1

---
+++ dzien1/zad4.java
@@ -0,0 +1,8 @@
+public class zad4 {
+    public static void main(String[] args) {
+        String imie = "Piotr";
+        String nazwisko = "Dragan";
+        System.out.println("Imię to: " + imie);
+        System.out.println("Nazwisko to: " + nazwisko);
+    }
+}
